bcdc8053a872f5169d3ff9d3ce911bb2fc2fbe07
AnthonyA490 <113041244+AnthonyA490@users.noreply.github.com>
2022-11-01 10:42:39 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(1)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    radio.sendNumber(3)
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(5)
+})
+// backwards
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(2)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    radio.sendNumber(4)
+})
+basic.showIcon(IconNames.Angry)
+radio.setGroup(10)
